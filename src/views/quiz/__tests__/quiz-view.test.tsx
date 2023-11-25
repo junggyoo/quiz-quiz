c75c71780a6a0dca81ae2d0d946d0b38274b6585
junggyoo <junggyoo.1@gmail.com>
2023-11-25 23:43:27 +0900

    test: QuizView 컴포넌트 에러, 로딩 완료 테스트 추가

--- src/views/quiz/__tests__/quiz-view.test.tsx
+++ src/views/quiz/__tests__/quiz-view.test.tsx
@@ -8,6 +8,7 @@ import { useQuizQuery } from '@/hooks/query/quiz';
 import { renderWithQuery } from '@/utils/render-with-query';
 
 import { server } from '@/mocks/api/server';
+import { MOCK_QUIZ_DATA } from '@/mocks/data/quiz';
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
@@ -27,5 +28,25 @@ describe('QuizView Component', () => {
     renderWithQuery(<QuizView />);
 
     expect(screen.getByTestId('quiz-loading')).toBeInTheDocument();
+  });
+
+  it('에러가 발생했을 때 <ErrorQuiz /> 컴포넌트가 렌더링 되어야 한다.', async () => {
+    (useQuizQuery as jest.Mock).mockImplementation(() => ({
+      isError: true,
+    }));
+
+    renderWithQuery(<QuizView />);
+
+    expect(screen.getByTestId('quiz-error')).toBeInTheDocument();
+  });
+
+  it('로딩이 완료 되면 퀴즈가 렌더링 되어야 한다.', async () => {
+    (useQuizQuery as jest.Mock).mockImplementation(() => ({
+      data: MOCK_QUIZ_DATA,
+    }));
+
+    renderWithQuery(<QuizView />);
+
+    expect(screen.getByText('HTC')).toBeInTheDocument();
   });
 });
--- src/views/quiz/components/error-quiz.tsx
+++ src/views/quiz/components/error-quiz.tsx
@@ -8,7 +8,7 @@ interface ErrorQuizProps {
 
 export default function ErrorQuiz({ onRetryClick }: ErrorQuizProps) {
   return (
-    <Container direction="col">
+    <Container direction="col" data-testid="quiz-error">
       <h1>문제를 불러오는데 실패했습니다.</h1>
       <Button
         size="lg"
